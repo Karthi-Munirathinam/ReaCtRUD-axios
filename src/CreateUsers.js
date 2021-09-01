9a3e7edc45af6a80bc892b2810bb5a8215fc59ce
Karthi <karthiraja268@gmail.com>
2021-09-01 21:54:32 +0530

    Responsive table

--- src/CreateUsers.js
+++ src/CreateUsers.js
@@ -31,37 +31,38 @@ function CreateUsers() {
         <div>
             <h3 className="page-title mb-4">Create New User</h3>
             {
-                isLoading ? <h1 className="loading">Loading...</h1> : (<form onSubmit={handleSubmit}>
-                    <div className="row mb-3">
-                        <div className="col-6">
-                            <label htmlFor="name">Name</label>
-                            <input type="text" value={name} onChange={(e) => setName(e.target.value)} id="name" className="form-control" />
+                isLoading ? <h1 className="loading">Loading...</h1>
+                    : (<form onSubmit={handleSubmit}>
+                        <div className="row">
+                            <div className="col-lg-6 mt-2 mb-2">
+                                <label htmlFor="name">Name</label>
+                                <input type="text" value={name} onChange={(e) => setName(e.target.value)} id="name" className="form-control" />
+                            </div>
+                            <div className="col-lg-6 mt-2 mb-2">
+                                <label htmlFor="age">Age</label>
+                                <input type="number" value={age} onChange={(e) => setAge(e.target.value)} id="age" className="form-control" />
+                            </div>
                         </div>
-                        <div className="col-6">
-                            <label htmlFor="age">Age</label>
-                            <input type="number" value={age} onChange={(e) => setAge(e.target.value)} id="age" className="form-control" />
+                        <div className="row">
+                            <div className="col-lg-6 mt-2 mb-2">
+                                <label htmlFor="email">E-mail</label>
+                                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} id="email" className="form-control" />
+                            </div>
+                            <div className="col-lg-6 mt-2 mb-2">
+                                <label htmlFor="imgurl">Image Url</label>
+                                <input type="text" value={imgurl} onChange={(e) => setImgurl(e.target.value)} id="imgurl" className="form-control" />
+                            </div>
                         </div>
-                    </div>
-                    <div className="row mb-3">
-                        <div className="col-6">
-                            <label htmlFor="email">E-mail</label>
-                            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} id="email" className="form-control" />
+                        <div className="row mb-4">
+                            <div className="col-12 mt-2 mb-2">
+                                <label htmlFor="description">Description</label>
+                                <textarea type="text" value={description} onChange={(e) => setDescription(e.target.value)} id="description" className="form-control" rows="3"></textarea>
+                            </div>
                         </div>
-                        <div className="col-6">
-                            <label htmlFor="imgurl">Image Url</label>
-                            <input type="text" value={imgurl} onChange={(e) => setImgurl(e.target.value)} id="imgurl" className="form-control" />
+                        <div className="btn-container">
+                            <button className="btn btn-outline-primary btn-container-primary" disabled={isLoading ? true : false}>Create User</button>
                         </div>
-                    </div>
-                    <div className="row mb-4">
-                        <div className="col-12">
-                            <label htmlFor="description">Description</label>
-                            <textarea type="text" value={description} onChange={(e) => setDescription(e.target.value)} id="description" className="form-control" rows="3"></textarea>
-                        </div>
-                    </div>
-                    <div className="btn-container">
-                        <button className="btn btn-outline-primary btn-container-primary" disabled={isLoading ? true : false}>Create User</button>
-                    </div>
-                </form>)
+                    </form>)
             }
 
         </div>
